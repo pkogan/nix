d3cbdc42cde65b71d786fe5dc09faf69c5634b14
pablo <pablo@yenni.fi.uncoma.edu.ar>
2019-12-12 16:29:12 -0300

    config Access Control

--- config/web.php
+++ config/web.php
@@ -22,7 +22,7 @@ $config = [
             'class' => 'yii\caching\FileCache',
         ],
         'user' => [
-            'identityClass' => 'app\models\User',
+            'identityClass' => 'app\models\Guardavidas',
             'enableAutoLogin' => true,
         ],
         'errorHandler' => [
